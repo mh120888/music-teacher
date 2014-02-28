c2a20a8c424d22c0cbc2c33aa0a1fce14dce0fcd
Apprentice <apprentice@dbc35.local>
2014-02-27 22:23:03 -0800

    Adding routes and controller methods for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MusicTeacherRails::Application.routes.draw do
   resources :users, only: [:new, :create, :show]
+
+  get '/signin' => 'users#signin', as: 'signin'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
