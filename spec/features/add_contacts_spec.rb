3c2b2b65a204c6dd08b07888f8dd59204c1fc156
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-01 12:22:58 -0800

    Modifies feature test for navigating to create contact page to reflect implementation of AJAX

--- spec/features/add_contacts_spec.rb
+++ spec/features/add_contacts_spec.rb
@@ -12,14 +12,19 @@ describe 'Adding contacts' do
     end
     it 'should allow me to navigate to the create contact page' do
       expect(page).to have_content 'Add New Contact'
-      click_on "Add New Contact"
+      click_on 'Add New Contact'
       expect(current_path).to eq new_contact_path
     end
   end
   describe 'contact creation form' do
     it 'should say "create contact"' do
-      visit new_contact_path
-      expect(page).to have_content 'Add New Contact'
+      visit contacts_path
+      click_on 'Add New Contact'
+      expect(page).to have_button 'Create Contact'
     end
+  end
+  describe 'viewing a specific contact' do
+  end
+  describe 'editing contacts' do
   end
 end
