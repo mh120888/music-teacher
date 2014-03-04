1f67a7fd44e372c7e78f56085a7a1bbd7691fb12
K Y <kevin@kevinyeh.com>
2014-03-03 17:27:19 -0800

    Clean up controller methods into before filters

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,42 +1,47 @@
 class ContactsController < ApplicationController
+  before_filter :get_user, only: [:index, :new, :create, :edit]
+  before_filter :get_contact, only: [:show, :edit, :update, :destroy]
+
   def index
-    @user = User.find(params[:user_id])
     @contacts = @user.contacts
   end
   def new
-    @user = User.find(params[:user_id])
     @contact = Contact.new
-    render partial: 'new'
+    render partial: 'new', :locals => { contact: @contact, user: @user }
   end
   def create
     @contact = Contact.new(params[:contact])
-    @contact.user_id = params[:user_id]
+    @contact.user = @user
     if @contact.save
-      @contacts = Contact.all
+      @contacts = @user.contacts
       render partial: 'list'
     else
-      @user = User.find(params[:user_id])
-      render partial: 'new'
+      render partial: 'new', :locals => { user: @user, contact: @contact }
     end
   end
   def show
-    @contact = Contact.find(params[:id])
     render partial: 'show', :locals => { contact: @contact }
   end
   def edit
-    @user = User.find(params[:user_id])
-    @contact = Contact.find(params[:id])
     render partial: 'edit', :locals => { user: @user, contact: @contact }
   end
   def update
-    @contact = Contact.find(params[:id])
     @contact.update_attributes(params[:contact])
     render partial: 'show', :locals => { contact: @contact }
   end
 
   def destroy
-    @contact = Contact.find(params[:id])
     @contact.destroy
     render :json => @contact.user_id
+  end
+
+  private
+
+  def get_user
+    @user = User.find(params[:user_id])
+  end
+
+  def get_contact
+    @contact = Contact.find(params[:id])
   end
 end
