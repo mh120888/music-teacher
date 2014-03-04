dbc15337a06052daf4e2ac28e4a4c8249c630cfb
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-03 20:18:52 -0800

    Modifies dashboard#index to include @user. Can now add user, edit user, and view users via ajax on dashboard. Caveats - user must first be signed in, delete user works but only after refreshing after missing template error.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @user = current_user
     @appointment = Appointment.new
     @appointments_by_date = Appointment.upcoming_grouped_by_date
     @date = Date.today
