3952d12d82986a23aea294c88919d3c359b5a3f4
Eli Shkurkin <eshkurkin@gmail.com>
2014-03-03 19:45:40 -0800

    Cleaned up a hige amount of CSS and JS, got rid of push state
    Much refactor, such clean

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,18 +5,7 @@ class DashboardController < ApplicationController
     @date = Date.today
   end
 
-  def index_partial
-    @appointment = Appointment.new
-    @appointments_by_date = Appointment.upcoming_grouped_by_date
-    @date = Date.today
-    render 'index', layout: false
-  end
-
   def lesson
     render 'dashboard/lesson/page'
-  end
-
-  def lesson_partial
-    render 'dashboard/lesson/page', layout: false
   end
 end
