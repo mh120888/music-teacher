e9f09133e2e7bc71e059bc34b13b2128fcab05fe
K Y <kevin@kevinyeh.com>
2014-03-04 17:31:41 -0800

    Add index controller test to payment profile

--- spec/controllers/payment_profiles_controller_spec.rb
+++ spec/controllers/payment_profiles_controller_spec.rb
@@ -29,14 +29,60 @@ describe PaymentProfilesController do
 
 	context '#index' do
 		let!(:payment_profile) { FactoryGirl.create(:payment_profile, user: user) }
-		before do
-			PaymentProfilesController.any_instance.stubs(:username).returns()
+		let!(:username) { "some name" }
+		let!(:charge_data) do
+			{ 
+				"data" => [
+					{
+						"created" => Time.now.to_i,
+						"card" => {
+							"name" => "shkurkin@thisislongstuff.com"
+							},
+						"amount" => 2000
+					}	
+				]
+			}
+		end
+		let!(:plan_data) do
+			{
+				"data" => [
+					{
+						"name" => "weekly lessons aaron blake",
+						"amount" => 2500,
+						"interval" => "week"
+					}
+				]
+			}
+		end
+		let!(:payments) { [mock('payment')] }
+
+		before(:each) do
+			PaymentProfilesController.any_instance.stubs(:username).returns(username)
+			PaymentProfilesController.stubs(:setup_client).returns(true)
+			Stripe::Charge.stubs(:all).returns(charge_data)
+			Stripe::Plan.stubs(:all).returns(plan_data)
+			PaymentProfile.any_instance.stubs(:payments).returns(payments)
 			get :index
 		end
 
-		it 'should store user instance' 
-		it 'should store connected instance'
-		it 'should store payments instance'
+		it 'should store user instance' do
+			expect(assigns(:user)).to eq username
+		end
 
+		it 'should store connected status' do
+			expect(assigns(:connected)).to be_true
+		end
+
+		it 'should store payments instance' do
+			expect(assigns(:payments)).to eq charge_data
+		end
+
+		it 'should store subscriptions instance' do
+			expect(assigns(:plans)).to eq plan_data
+		end
+
+		it 'should store outstanding payments' do
+			expect(assigns(:outstanding).length).to eq payments.length
+		end
 	end
 end
