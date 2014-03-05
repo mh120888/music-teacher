ca873555bdd40f87f0287924485b4b3c249b2e84
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-05 14:56:43 -0800

    Fixes bug with dashboard displaying all Contacts (students); now shows only current user's contacts/students.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,11 +5,12 @@ class DashboardController < ApplicationController
       @appointment = Appointment.new
       @appointments_by_date = Appointment.upcoming_grouped_by_date(current_user)
       @date = Date.today
-      @contacts = Contact.all
+      @contacts = @user.contacts
       @contact = Contact.new
       @assignments = current_user.get_recent_assignments
       @pending = current_user.get_pending_assignments
       @feed = (@assignments + @pending).sort_by(&:created_at).reverse
+      binding.pry
     end
   end
 
