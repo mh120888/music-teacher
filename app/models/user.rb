5097575c186e84ef8a3afdcf5910b8ade6bc665a
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-05 17:27:36 -0800

    Seed data, two users created, one with no students and one with two students (two students/contacts are also created)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   include BCrypt
   before_create :create_remember_token
 
-  attr_accessible :name, :email, :student_id, :password
+  attr_accessible :name, :email, :student_id
   has_many :appointments
   has_many :contacts
   has_many :payment_profiles
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 users = [
-  {name: 'Andy Lee', password: 'password', email: 'andy@example.com'},
-  {name: 'Eli Shkurkin', password: 'password', email:'eli@example.com'}
+  {name: 'Andy Lee', email: 'andy@example.com'},
+  {name: 'Eli Shkurkin', email:'eli@example.com'}
 ]
 
-users.each { |params| User.create(params) }
+users.each do |params| 
+  user = User.new(params)
+  user.password = 'password'
+  user.save
+end
 
 student_contacts = [
-  
+  {first_name: 'Kevin', last_name: 'Yeh', email:'kevin@example.com', address: '101 Main Street, Berkeley, CA', phone:'999-555-1234'},
+  {first_name: 'Matt', last_name: 'Higgins', email:'matt@example.com', address: '102 Main Street, Berkeley, CA', phone:'810-555-1239'}
 ]
+
+user = User.where('name=?', 'Andy Lee').first
+
+student_contacts.each do |params| 
+  contact = Contact.new(params)
+  user.contacts << contact
+  contact.save
+end
